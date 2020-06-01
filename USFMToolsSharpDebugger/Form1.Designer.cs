f4382363d1ed3632549a0d0197e7c3ce4ef1fd2d
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2020-06-01 14:04:21 -0400

    Initial Commit

---
+++ USFMToolsSharpDebugger/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace USFMToolsSharpDebugger
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trOutput = new System.Windows.Forms.TreeView();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // trOutput
+            // 
+            this.trOutput.Location = new System.Drawing.Point(417, 56);
+            this.trOutput.Name = "trOutput";
+            this.trOutput.Size = new System.Drawing.Size(386, 426);
+            this.trOutput.TabIndex = 0;
+            this.trOutput.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.trOutput_AfterSelect);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 56);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(399, 426);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input";
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDetails.Location = new System.Drawing.Point(814, 58);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(269, 423);
+            this.lblDetails.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 532);
+            this.Controls.Add(this.lblDetails);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.trOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView trOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDetails;
+    }
+}
+
---
+++ USFMToolsSharpDebugger/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using USFMToolsSharp;
+using USFMToolsSharp.Models.Markers;
+
+namespace USFMToolsSharpDebugger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            USFMParser parser = new USFMParser();
+            var output = parser.ParseFromString(txtInput.Text);
+            trOutput.Nodes.Clear();
+            foreach(var marker in output.Contents)
+            {
+                trOutput.Nodes.Add(CreateTreeNode(marker));
+            }
+        }
+
+        private TreeNode CreateTreeNode(Marker marker)
+        {
+            TreeNode output = new TreeNode();
+            output.Text = marker.GetType().Name;
+            output.Tag = marker;
+            foreach(var node in marker.Contents)
+            {
+                output.Nodes.Add(CreateTreeNode(node));
+            }
+            return output;
+        }
+
+        private string CreateDetail(Marker marker)
+        {
+            switch (marker)
+            {
+                case TextBlock textBlock:
+                    return $"Text: {textBlock.Text}";
+                case FMarker fMarker:
+                    return $"Caller: {fMarker.FootNoteCaller}";
+                case VMarker vMarker:
+                    return $"Verse Number: {vMarker.VerseNumber}";
+            }
+            return "";
+        }
+
+        private void trOutput_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            lblDetails.Text = CreateDetail((Marker)e.Node.Tag);
+        }
+    }
+}
